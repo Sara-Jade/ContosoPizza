c617cac30934e0d061830dff7883b19509a6b0ae
Sara-Jade <88349392+Sara-Jade@users.noreply.github.com>
2024-03-26 23:59:00 +0000

    Need to remove a product

--- parts/1-getting-started/ContosoPizza/Program.cs
+++ parts/1-getting-started/ContosoPizza/Program.cs
@@ -19,6 +19,32 @@ using ContosoPizzaContext context = new ContosoPizzaContext();
 
 // context.SaveChanges();
 
+// var products = context.Products
+//     .Where(p => p.Price > 10)
+//     .OrderBy(p => p.Name);
+
+// foreach (var product in products)
+// {
+//     Console.WriteLine($"Id: {product.Id}");
+//     Console.WriteLine($"Name: {product.Name}");
+//     Console.WriteLine($"Price: {product.Price}");
+//     Console.WriteLine(new string('-', 20));
+//     Console.WriteLine();
+// }
+
+// var veggieSpecial = context.Products
+//                        .Where(p => p.Name == "Veggie Special Pizza")
+//                        .FirstOrDefault();
+
+// if (veggieSpecial is Product)
+// {
+//     veggieSpecial.Price = 10.99M;
+// }
+
+// context.Remove(veggieSpecial);
+
+// context.SaveChanges();
+
 var products = context.Products
     .Where(p => p.Price > 10)
     .OrderBy(p => p.Name);
@@ -31,14 +57,3 @@ foreach (var product in products)
     Console.WriteLine(new string('-', 20));
     Console.WriteLine();
 }
-
-var veggieSpecial = context.Products
-                       .Where(p => p.Name == "Veggie Special Pizza")
-                       .FirstOrDefault();
-
-if (veggieSpecial is Product)
-{
-    veggieSpecial.Price = 10.99M;
-}
-
-context.SaveChanges();
