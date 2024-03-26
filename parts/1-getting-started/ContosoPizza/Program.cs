e6d8a54df0b494356cbbb2a35a106714ede22e9b
Sara-Jade <88349392+Sara-Jade@users.noreply.github.com>
2024-03-26 23:52:42 +0000

    Need pizzas and updates

--- parts/1-getting-started/ContosoPizza/Program.cs
+++ parts/1-getting-started/ContosoPizza/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+// Product veggieSpecial = new Product()
+// {
+//     Name = "Veggie Special Pizza",
+//     Price = 9.99M
+// };
+// context.Products.Add(veggieSpecial);
+
+// Product deluxeMeat = new Product()
+// {
+//     Name = "Deluxe Meat Pizza",
+//     Price = 12.99M
+// };
+// context.Add(deluxeMeat);
+
+// context.SaveChanges();
+
+var products = context.Products
+    .Where(p => p.Price > 10)
+    .OrderBy(p => p.Name);
+
+foreach (var product in products)
+{
+    Console.WriteLine($"Id: {product.Id}");
+    Console.WriteLine($"Name: {product.Name}");
+    Console.WriteLine($"Price: {product.Price}");
+    Console.WriteLine(new string('-', 20));
+    Console.WriteLine();
+}
+
+var veggieSpecial = context.Products
+                       .Where(p => p.Name == "Veggie Special Pizza")
+                       .FirstOrDefault();
+
+if (veggieSpecial is Product)
+{
+    veggieSpecial.Price = 10.99M;
+}
+
+context.SaveChanges();
---
+++ parts/1-getting-started/ContosoPizza/top1000products.sql
@@ -0,0 +1,4 @@
+SELECT TOP (1000) [Id]
+      ,[Name]
+      ,[Price]
+  FROM [ContosoPizza-Part1].[dbo].[Products]
